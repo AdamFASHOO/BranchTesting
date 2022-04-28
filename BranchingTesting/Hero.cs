e6d177f9aae3c6cedaa13cefe6dbdc1d0ed56130
AdamFASHOO <AdamChapman1994@gmail.com>
2022-04-28 10:02:16 -0400

    adding everything else

---
+++ BranchingTesting/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingExample
+{
+    internal class Hero
+    {
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public Hero(int HP, int Attack)
+        {
+            this.HP = HP;
+            this.Attack = Attack;
+        }
+    }
+}
